874093fd5f86c68f679cd9144dc3bf54f2ee0083
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-18 19:20:55 +0100

    Ajout de la classe principale pour l'application Spring Boot Eventify Spring Security

---
+++ src/main/java/com/eventify/springsecurity/EventifySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.eventify.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifySpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventifySpringSecurityApplication.class, args);
+    }
+
+}
