e922b7b747829ee84f4ba02fbf6d52ae4642ea1f
salma hamdi <saalmahm51@gmail.com>
2025-11-20 20:04:19 +0100

    ES-8 : Définition de l’interface UserService

---
+++ src/main/java/com/eventify/springsecurity/service/UserService.java
@@ -0,0 +1,22 @@
+package com.eventify.springsecurity.service;
+
+import com.eventify.springsecurity.dto.ChangeRoleRequest;
+import com.eventify.springsecurity.dto.UserCreateDTO;
+import com.eventify.springsecurity.dto.UserResponseDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    UserResponseDTO createUser(UserCreateDTO dto);
+
+    UserResponseDTO getUserById(Long id);
+
+    List<UserResponseDTO> getAllUsers();
+
+    UserResponseDTO updateUser(Long id, UserCreateDTO dto);
+
+    void deleteUser(Long id);
+
+    UserResponseDTO changeUserRole(Long userId, ChangeRoleRequest request);
+}
