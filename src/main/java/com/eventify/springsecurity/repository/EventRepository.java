62816b53d0e98ad80a5da562dc7bb0954945b2a5
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-20 21:28:01 +0100

    ES-18 Ajout de l'interface EventRepository pour gérer les opérations liées aux événements

---
+++ src/main/java/com/eventify/springsecurity/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package com.eventify.springsecurity.repository;
+
+import com.eventify.springsecurity.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    
+}
+
