f01132042893d04aea3cb92248195e43b474613b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-20 20:34:05 +0100

    ES-8 Ajout de validations pour les champs de la classe User

--- src/main/java/com/eventify/springsecurity/entity/User.java
+++ src/main/java/com/eventify/springsecurity/entity/User.java
@@ -2,9 +2,15 @@ package com.eventify.springsecurity.entity;
 
 import com.eventify.springsecurity.enums.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -17,15 +23,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Le nom est requis")
+    @Column(nullable = false)
     private String name;
 
+    @Email(message = "L'email doit être valide")
+    @NotBlank(message = "L'email est requis")
     @Column(unique = true, nullable = false)
     private String email;
 
+    @NotBlank(message = "Le mot de passe est requis")
     @Column(nullable = false)
     private String password;
 
+    @NotNull(message = "Le rôle est requis")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    
 }
