a521913240cf8ada4b54c1ec955cbc60c84d6079
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-20 19:46:59 +0100

    ES-18 Création de la classe Event avec annotations JPA et Lombok

---
+++ src/main/java/com/eventify/springsecurity/entity/Event.java
@@ -0,0 +1,22 @@
+package com.eventify.springsecurity.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    
+
+}
+
