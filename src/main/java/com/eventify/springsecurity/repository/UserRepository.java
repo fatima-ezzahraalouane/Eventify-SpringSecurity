7478f81035ea29253b5a3f86f5c88efc7963ae71
salma hamdi <saalmahm51@gmail.com>
2025-11-20 19:57:59 +0100

    ES-8 :  Création de UserRepository (Spring Data JPA)

---
+++ src/main/java/com/eventify/springsecurity/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.eventify.springsecurity.repository;
+
+import com.eventify.springsecurity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
