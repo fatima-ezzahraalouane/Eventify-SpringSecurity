90a556b207c8e36aafaa73b67ccfbb6a0c7f8b7c
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-20 21:13:22 +0100

    ES-23 Ajout de l'exception UnauthorizedActionException pour gérer les actions non autorisées

---
+++ src/main/java/com/eventify/springsecurity/exception/UnauthorizedActionException.java
@@ -0,0 +1,8 @@
+package com.eventify.springsecurity.exception;
+
+public class UnauthorizedActionException extends RuntimeException {
+    public UnauthorizedActionException(String message) {
+        super(message);
+    }
+}
+
