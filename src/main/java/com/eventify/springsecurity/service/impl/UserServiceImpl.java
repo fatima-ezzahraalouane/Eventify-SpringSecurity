871432f6e585a2a2b86fd865c8e503c38898b0e5
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-20 21:08:03 +0100

    ES-8 Ajout du constructeur dans UserServiceImpl pour initialiser les dépendances

--- src/main/java/com/eventify/springsecurity/service/impl/UserServiceImpl.java
+++ src/main/java/com/eventify/springsecurity/service/impl/UserServiceImpl.java
@@ -24,5 +24,13 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
+    public UserServiceImpl(UserRepository userRepository,
+                           UserMapper userMapper,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     
 }
