22850f6b9d8997403521a99c423d2c5cc0bfa50a
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:35:59 +0100

    ES-13 Ajout de la propriété userRepository et du constructeur dans la classe CustomUserDetailsService pour l'injection de dépendance.

--- src/main/java/com/eventify/springsecurity/security/CustomUserDetailsService.java
+++ src/main/java/com/eventify/springsecurity/security/CustomUserDetailsService.java
@@ -16,6 +16,12 @@ import java.util.Collections;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     
 }
 
