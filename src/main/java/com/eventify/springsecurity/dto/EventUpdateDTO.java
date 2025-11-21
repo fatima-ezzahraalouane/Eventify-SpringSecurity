f9ddb7c25b5cbbe359ef19a22b978e15a04a1d54
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:04:06 +0100

    ES-18 Ajout de la classe EventUpdateDTO pour gérer la mise à jour des événements avec validation

---
+++ src/main/java/com/eventify/springsecurity/dto/EventUpdateDTO.java
@@ -0,0 +1,20 @@
+package com.eventify.springsecurity.dto;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventUpdateDTO {
+
+    
+}
+
