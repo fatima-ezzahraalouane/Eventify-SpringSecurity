f96b6e3e8c442f8f21454fc8569664673fa0b65b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:30:17 +0100

    ES-13 Ajout de la classe SecurityConfig pour configurer la sécurité de l'application.

---
+++ src/main/java/com/eventify/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.eventify.springsecurity.config;
+
+import com.eventify.springsecurity.security.CustomAccessDeniedHandler;
+import com.eventify.springsecurity.security.CustomAuthenticationEntryPoint;
+import com.eventify.springsecurity.security.CustomAuthenticationProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    
+}
+
