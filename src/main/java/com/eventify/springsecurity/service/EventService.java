a234a44530530b19fac30f35e046ef79a0f6ca31
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:19:35 +0100

    ES-18 Ajout de l'interface EventService pour la gestion des événements, incluant les DTOs pour la création, la mise à jour et la réponse des événements.

---
+++ src/main/java/com/eventify/springsecurity/service/EventService.java
@@ -0,0 +1,13 @@
+package com.eventify.springsecurity.service;
+
+import com.eventify.springsecurity.dto.EventCreateDTO;
+import com.eventify.springsecurity.dto.EventResponseDTO;
+import com.eventify.springsecurity.dto.EventUpdateDTO;
+
+import java.util.List;
+
+public interface EventService {
+
+    
+}
+
