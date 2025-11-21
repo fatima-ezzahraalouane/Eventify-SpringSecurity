95f421b2ca37dc4b6a90cb8b54f238080428f335
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:20:44 +0100

    ES-18 Ajout des méthodes de gestion des événements dans l'interface EventService

--- src/main/java/com/eventify/springsecurity/service/EventService.java
+++ src/main/java/com/eventify/springsecurity/service/EventService.java
@@ -8,6 +8,16 @@ import java.util.List;
 
 public interface EventService {
 
-    
+    List<EventResponseDTO> getAllEvents();
+
+    EventResponseDTO getEventById(Long id);
+
+    EventResponseDTO createEvent(EventCreateDTO dto, Long organizerId);
+
+    EventResponseDTO updateEvent(Long id, EventUpdateDTO dto, Long organizerId);
+
+    void deleteEvent(Long id, Long organizerId);
+
+    void deleteEventByAdmin(Long id);
 }
 
