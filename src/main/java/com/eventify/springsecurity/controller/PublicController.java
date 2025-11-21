b3793738fbb868e867b5b7c53fa6ea1ad697036d
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:01:07 +0100

    ES-8 Ajout de la classe PublicController pour gérer les requêtes publiques de l'API

---
+++ src/main/java/com/eventify/springsecurity/controller/PublicController.java
@@ -0,0 +1,21 @@
+package com.eventify.springsecurity.controller;
+
+import com.eventify.springsecurity.dto.EventResponseDTO;
+import com.eventify.springsecurity.dto.UserCreateDTO;
+import com.eventify.springsecurity.dto.UserResponseDTO;
+import com.eventify.springsecurity.service.EventService;
+import com.eventify.springsecurity.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/public")
+public class PublicController {
+
+    
+}
+
