4a236b5ed89d1f0a8dc70db3ab9fbe2c0624ae36
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 11:53:52 +0100

    ES-8 Ajout de la classe OrganizerController pour gérer les opérations liées aux organisateurs

---
+++ src/main/java/com/eventify/springsecurity/controller/OrganizerController.java
@@ -0,0 +1,22 @@
+package com.eventify.springsecurity.controller;
+
+import com.eventify.springsecurity.dto.EventCreateDTO;
+import com.eventify.springsecurity.dto.EventResponseDTO;
+import com.eventify.springsecurity.dto.EventUpdateDTO;
+import com.eventify.springsecurity.dto.UserResponseDTO;
+import com.eventify.springsecurity.service.EventService;
+import com.eventify.springsecurity.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/organizer")
+public class OrganizerController {
+
+    
+}
+
