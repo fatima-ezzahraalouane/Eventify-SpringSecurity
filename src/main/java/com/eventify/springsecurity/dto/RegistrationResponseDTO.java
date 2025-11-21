689ea8ec70cb5d2de9763f32ee53b4ed811e128c
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:06:15 +0100

    ES-18 Ajout de la classe RegistrationResponseDTO pour représenter les réponses d'enregistrement

---
+++ src/main/java/com/eventify/springsecurity/dto/RegistrationResponseDTO.java
@@ -0,0 +1,15 @@
+package com.eventify.springsecurity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationResponseDTO {
+    
+}
+
