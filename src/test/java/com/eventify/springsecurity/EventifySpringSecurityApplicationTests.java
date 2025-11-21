12366e926690b2e6662f89c45bfd1ab959fcc42c
salma hamdi <saalmahm51@gmail.com>
2025-11-21 23:11:12 +0100

    ES-30 test: activer le profil test pour les tests Spring Boot

--- src/test/java/com/eventify/springsecurity/EventifySpringSecurityApplicationTests.java
+++ src/test/java/com/eventify/springsecurity/EventifySpringSecurityApplicationTests.java
@@ -1,13 +1,15 @@
+// java
 package com.eventify.springsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventifySpringSecurityApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
