b0ffb3bd8274dfae6a6e3794772fa3fd2e36bd47
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 11:45:40 +0100

    ES-8 Ajout de la classe UserController pour gérer les requêtes liées aux utilisateurs

---
+++ src/main/java/com/eventify/springsecurity/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.eventify.springsecurity.controller;
+
+import com.eventify.springsecurity.dto.RegistrationResponseDTO;
+import com.eventify.springsecurity.dto.UserResponseDTO;
+import com.eventify.springsecurity.service.RegistrationService;
+import com.eventify.springsecurity.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    
+}
+
