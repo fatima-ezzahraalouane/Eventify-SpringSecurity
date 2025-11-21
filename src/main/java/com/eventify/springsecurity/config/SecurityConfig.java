d06beaf026cb36c5284614cae7b7fb42ed49113f
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 14:12:01 +0100

    ES-30 Ajout de l'annotation @Profile pour exclure la configuration de sécurité en mode test

--- src/main/java/com/eventify/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/eventify/springsecurity/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.eventify.springsecurity.security.CustomAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -21,6 +22,7 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@Profile("!test")
 public class SecurityConfig {
 
     private final CustomAuthenticationProvider customAuthenticationProvider;
