d617156fd25762b19d962efe3125f368e6319762
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:18:47 +0100

    ES-18 Ajout de l'interface RegistrationMapper pour la gestion des mappages entre les entités et les DTOs d'inscription.

---
+++ src/main/java/com/eventify/springsecurity/mapper/RegistrationMapper.java
@@ -0,0 +1,13 @@
+package com.eventify.springsecurity.mapper;
+
+import com.eventify.springsecurity.dto.RegistrationResponseDTO;
+import com.eventify.springsecurity.entity.Registration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RegistrationMapper {
+
+    
+}
+
