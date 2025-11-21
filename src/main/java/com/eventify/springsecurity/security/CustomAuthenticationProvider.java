c670f6cdb682a417b40c4778a7d755664c31bcd3
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:33:58 +0100

    ES-13 Ajout de la classe CustomAuthenticationProvider pour gérer l'authentification personnalisée.

---
+++ src/main/java/com/eventify/springsecurity/security/CustomAuthenticationProvider.java
@@ -0,0 +1,18 @@
+package com.eventify.springsecurity.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    
+}
+
