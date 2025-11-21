9b3b0e1855f755be0a4b137d0ea80ef66aaa95aa
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 11:50:53 +0100

    ES-8 Ajout de la classe AdminController pour gérer les opérations administratives

---
+++ src/main/java/com/eventify/springsecurity/controller/AdminController.java
@@ -0,0 +1,20 @@
+package com.eventify.springsecurity.controller;
+
+import com.eventify.springsecurity.dto.ChangeRoleRequest;
+import com.eventify.springsecurity.dto.UserResponseDTO;
+import com.eventify.springsecurity.service.EventService;
+import com.eventify.springsecurity.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    
+}
+
