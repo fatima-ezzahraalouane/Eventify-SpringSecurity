9a65de2e3897af9fbc55abd85f52e5c1cba3186a
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:04:54 +0100

    ES-18 Ajout de l'interface EventMapper pour la gestion des mappages entre les DTO et l'entité Event

---
+++ src/main/java/com/eventify/springsecurity/mapper/EventMapper.java
@@ -0,0 +1,16 @@
+package com.eventify.springsecurity.mapper;
+
+import com.eventify.springsecurity.dto.EventCreateDTO;
+import com.eventify.springsecurity.dto.EventResponseDTO;
+import com.eventify.springsecurity.dto.EventUpdateDTO;
+import com.eventify.springsecurity.entity.Event;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface EventMapper {
+
+    
+}
+
