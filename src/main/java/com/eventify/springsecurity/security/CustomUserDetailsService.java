9d86c77bab31b89ee2b124dac93fe7fcac89c6dd
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-21 22:35:35 +0100

    ES-13 Ajout de la classe CustomUserDetailsService pour gérer les détails des utilisateurs dans le cadre de l'authentification.

---
+++ src/main/java/com/eventify/springsecurity/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.eventify.springsecurity.security;
+
+import com.eventify.springsecurity.entity.User;
+import com.eventify.springsecurity.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    
+}
+
